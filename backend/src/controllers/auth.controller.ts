8e5a88e8aa0fd89837582afeb32dc01997cc5dba
Federico Giorgino <59868572+federicogiorgino@users.noreply.github.com>
2024-12-11 23:58:48 +0100

    feat(backend): /auth/logout endpoint

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,8 +1,10 @@
 import { CREATED, OK } from "../constants/http";
+import SessionModel from "../models/session.model";
 import { loginSchema, registerSchema } from "../schemas/auth.schemas";
 import { createAccount, loginUser } from "../services/auth.service";
 import catchErrors from "../utils/catch-errors";
-import { setAuthCookies } from "../utils/cookies";
+import { clearAuthCookies, setAuthCookies } from "../utils/cookies";
+import { verifyToken } from "../utils/jwt";
 
 export const registerHandler = catchErrors(async (req, res) => {
   const request = registerSchema.parse({
@@ -28,4 +30,19 @@ export const loginHandler = catchErrors(async (req, res) => {
   return setAuthCookies({ res, accessToken, refreshToken })
     .status(OK)
     .json({ message: "Login successful" });
+});
+
+export const logoutHandler = catchErrors(async (req, res) => {
+  const accessToken = req.cookies.accessToken as string | undefined;
+  const { payload } = verifyToken(accessToken || "");
+
+  if (payload) {
+    // remove session from db
+    await SessionModel.findByIdAndDelete(payload.sessionId);
+  }
+
+  // clear cookies
+  return clearAuthCookies(res)
+    .status(OK)
+    .json({ message: "Logout successful" });
 });
